7f643450c374aa98954c0307778151d46d9782ff
yofio <d.y215jump@i.softbank.jp>
2021-07-30 16:38:26 +0900

    Banner.tsxとBanner.scssを作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { Row } from "./components/Row";
 import { requests } from "./request";
+import { Banner } from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGUINALS"
         fetchUrl={requests.feachNetflixOriginals}
