bd314d1f5505ff6f7a985d67a452b4396c5b329a
yofio <d.y215jump@i.softbank.jp>
2021-07-31 16:46:45 +0900

    NavのSCSSを作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Row } from "./components/Row";
 import { requests } from "./request";
 import { Banner } from "./components/Banner";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGUINALS"
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./Nav.scss";
 
 type Props = {
   className?: string;
